d7ee472a878332229d9905b5b91d8841dd5767bd
p1st1s <emmanueladeboye2017@gmail.com>
2019-08-31 02:31:54 +0100

    - commit modification to a prev file main.py though i will soon drop it

--- main/main.py
+++ main/main.py
@@ -7,55 +7,25 @@ import threading
 import playsound
 #from playsound import playsound
 
-class Reminder(eaTime.Appointment):
+class CountDown(eaTime.Reminder):
     """
+    Class CountDown is the subclass of the Reminder class.
 
-    """
-    def __init__(self, g, t, l = None):
-        self.__goal = g
-        self.__dtime = t # due time
-        self.__location = l
-    
-    # ----------- setters ---------
-    def set_time(self, nt):
-        """ nt is new time """
-        self.__dtime = nt
-    
-    def set_goal(self, ng):
-        """ ng is new goal """
-        self.__goal = ng
-
-    def set_loc(self, nl):
-        """ nl is new location """
-        self.__location = nl
-
-    # ----------- getters ------------
-    def get_time(self):
-        return self.__dtime
-
-    def get_goal(self):
-        return self.__goal
-
-    def get_loc(self):
-        return self.__location
-
-class Alarm(Reminder):
-    """
-    Class alarm is the subclass of the Reminder class.
-
-    it contains method to calculate when the appointment
-    will be due and when to sound the alarm.
+    it contains method that calculate when the timer will
+    run out and sound the alarm at an appropriate time.
 
     Methods include:
-    ringer - plays the alarm audio file
+    ringer - plays an audio file when the timer runs out
     ring_time - calculates the time to sound the ringer
     """
     def __init__(self, g, t, f='m', l=None):
         super().__init__(g, t, l)
-        '''
-        f is the format of the time in either
-        minutes, secs or hours)
-        '''
+        """
+        - f is the format of the time in either
+          minutes, secs or hours)
+        - g = goal/purpose of the countdown
+        - t = due time
+        """
         self.tf = f #time format
 
     def __ringer(self):
@@ -80,10 +50,14 @@ class Alarm(Reminder):
         self.__ring_time()
 
 
-class my_thread(threading.Thread):
+class CountDownThread(threading.Thread):
+    """
+    A class that allow us to run more than one instance
+    of the CountDown class concurrently
+    """
     def __init__(self, g, t, f='m', l=None):
         threading.Thread.__init__(self)
-        self.name = g # Thread name
+        self.name = g # Thread name/label
         self.t = t # time/ duration
         self.f = f # alarm time format
         self.g = g # goal
@@ -91,7 +65,7 @@ class my_thread(threading.Thread):
 
     def run(self):
         print("starting " + self.name)
-        Alarm(self.g, self.t, self.f, self.l).run()
+        CountDown(self.g, self.t, self.f, self.l).run()
         print("Exiting " + self.name)
 
 
@@ -109,8 +83,8 @@ if __name__ == "__main__":
     #a = Alarm('buy stat book', 10, f='s')
 
     # test threading
-    thread1 = my_thread('10 secs test', 10, 's')
-    thread2 = my_thread('20 secs test', 30, 's')
+    thread1 = CountDownThread('10 secs test', 10, 's')
+    thread2 = CountDownThread('20 secs test', 20, 's')
     # start the thread
     thread1.start()
     thread2.start()
