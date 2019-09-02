f62fb4aca9db5e3632f24b35228b3f683491e7a0
p1st1s <emmanueladeboye2017@gmail.com>
2019-09-02 12:26:01 +0100

    - implement threading so that two alarms can run concurrently

--- code/alarm.py
+++ code/alarm.py
@@ -5,7 +5,8 @@ from playsound import playsound
 
 class Alarm:
     """
-    class alarm defines an alarm clock app with various functionalities.'
+    class alarm defines an alarm clock app with various
+    functionalities.'
     """
     def __init__(self, t, title='nil'):
         try:
@@ -15,14 +16,15 @@ class Alarm:
 
         # when an integer is passed as a time
         except AttributeError:
-            raise ValueError("""
-            Time (t) must be a string, so enclose the time in '' separating
-            the hours and mins with a colon (:). e.g '12:00'
-            """)
+            raise ValueError("Time (t) must be a string"+\
+                ", so enclose the time in '' separating"+\
+                    " the hours and mins with a colon "+\
+                        "(:). e.g '12:00'")
 
         # when time is not enclosed in a quote
         except SyntaxError:
-            raise ValueError('invalid input! time(t) and title must be a string.')
+            raise ValueError("invalid input! time(t) "+\
+                "and title must be a string.")
 
         # when the title is not surrounded with a quote
         except NameError:
@@ -30,23 +32,32 @@ class Alarm:
 
     def proc(self):
         """
-        contains all process that keep record of the alarm/reminder
+        contains all process that keep record of the
+        alarm/reminder
         """
         # get current time as a tuple
         now = time.localtime()
 
         # compare current time with the entered time
-        # 
-        # if current time (h) > due time (h)
-        if now[3] > int(self.tm[0]):
-            raise ValueError('invalid time! That time has passed.')
+        #
+        # if min or secs > 59
+        if int(self.tm[1]) > 59:
+            raise ValueError("invalid time!")
+        # if current time (h) > due time
+        elif now[3] > int(self.tm[0]):
+            raise ValueError("invalid time! That time "+\
+                "has passed.")
 
         # hour is the same, but mins is lesser than current mins
-        elif now[3] == int(self.tm[0]) and now[4] > int(self.tm[1]):
-            raise ValueError('invalid time! That time has passed.')
+        elif now[3] == int(self.tm[0]) and now[4] >\
+            int(self.tm[1]):
+            raise ValueError("invalid time! That time "+\
+                "has passed.")
 
         # hour and mins are equal
-        elif now[3] == int(self.tm[0]) and now[4] == int(self.tm[1]):
+        elif now[3] == int(self.tm[0]) and now[4] ==\
+            int(self.tm[1]):
+            # set sleep duration to zero
             sleep_dur = 0
         # if current time > due time
         else:
@@ -56,17 +67,28 @@ class Alarm:
             # convert h and m to seconds
             sleep_dur = Timing().hs(h) + Timing().ms(m)
 
-        # Tell the user when the alarm will sound
-        if sleep_dur < 60:    
-            print(f"Alarm will sound in {sleep_dur} second(s) time")
+        #----------- INFORM USERS OF WAIT TIME --------
+        # sleep duration less than 60 secs
+        if sleep_dur < 60:
+            # Tell the user when the alarm will sound  
+            print(f'Alarm will sound in {sleep_dur} '+\
+                'second(s) time')
 
-        elif sleep_dur > 60 and sleep_dur < 3600:
-            st = Timing().sm(sleep_dur, r='t') #sleep time
-            print(f"Alarm will sound in {st[0]} min(s) {st[1]} seconds time")
+        # sleep duration > 60 secs and < 3600
+        elif sleep_dur >= 60 and sleep_dur < 3600:
+            #sleep time
+            st = Timing().sm(sleep_dur, r='t')
+            # Tell the user when the alarm will sound
+            print(f'Alarm will sound in {st[0]} min(s)'+\
+                ' {st[1]} seconds time')
             
-        elif sleep_dur > 3600:
+        # sleep duration > 3600
+        elif sleep_dur >= 3600:
+            #sleep time
             st = Timing().sh(sleep_dur, r='t')
-            print(f"Alarm will sound in {st[0]} hour(s) {st[1]} min(s) time")
+            # Tell the user when the alarm will sound
+            print(f"Alarm will sound in {st[0]} hour(s)"+\
+                " {st[1]} min(s) time")
         
         # sleep till its time to sound the alarm
         time.sleep(sleep_dur)
@@ -87,6 +109,23 @@ class Alarm:
         self.ring()
 
 
+class AlarmThread(threading.Thread):
+    def __init__(self, t, title):
+        """
+        constructor for AlarmThread class.
+        t - stands for time(t)
+        title - is the title of the alarm
+        """
+        threading.Thread.__init__(self)
+        self.time = t
+        self.title = title
+
+    def run(self):
+        print(f'starting {self.title}!')
+        Alarm(self.time, self.title).start()
+        print(f'exiting {self.title}!')
+
+
 class Timer:
     """
     class Timer defines a countdown timer
@@ -100,4 +139,6 @@ class Timer:
 
 # ---------------- test -------------
 if __name__ == "__main__":
-    Alarm('13:58', 'test').start()
+    AlarmThread('11:60', 't1').start()
+    AlarmThread('11:21', 't2').start()
+   
